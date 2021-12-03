e2e8cda373aebb9ba43eabe4b699cdbf0c04f3d9
NikitaSamkov <nick.samkov@yandex.ru>
2021-12-03 15:10:16 +0500

    added SignOut

--- index.php
+++ index.php
@@ -11,11 +11,16 @@ session_start();
 </head>
 
 <body>
-<?php if(isset($_SESSION['user'])): ?>
-<p>Здравствуйте, <?=$_SESSION['user']['firstname']?></p>
-<?php endif; ?>
-<a href="register.php">Зарегистрироваться</a>
-<a href="login.php">Войти</a>
+<header>
+    <?php if(isset($_SESSION['user'])): ?>
+        <p>Здравствуйте, <?=$_SESSION['user']['firstname']?></p>
+        <a href="SignOut.php">Выйти</a>
+    <?php else: ?>
+        <a href="login.php">Войти</a>
+    <?php endif; ?>
+    <a href="register.php">Зарегистрироваться</a>
+</header>
+
 </body>
 
 </html>
