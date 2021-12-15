ed29c53b2b5d01433d260fa1d5f7e7fd02b2b3fa
NikitaSamkov <nick.samkov@yandex.ru>
2021-12-15 14:43:08 +0500

    edit_profile page title fix

--- data/edit_profile.php
+++ data/edit_profile.php
@@ -8,7 +8,7 @@ session_start();
 
 <head>
     <meta charset='utf-8'>
-    <title>Регистрация</title>
+    <title>Редактировать профиль</title>
     <link rel="stylesheet" type="text/css" href="../auth/auth.css">
     <link rel="stylesheet" type="text/css" href="edit.css">
 </head>
